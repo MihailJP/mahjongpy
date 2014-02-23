0e103d4fac9f76b3ae9e81e236ff590c0f14c228
TANIGAWA Takashi <mihailjp@gmail.com>
2014-02-23 16:36:07 +0900

    Add setup.py and blank __init__.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name           = 'MahjongPY',
+      version        = '0.0.1',
+      description    = 'Python library to do with Mahjong tiles',
+      author         = 'MihailJP',
+      packages       = 'mahjongpy',
+      license        = 'MIT License',
+      url            = 'https://github.com/MihailJP/mahjongpy',
+      classifiers    = [
+         "Programming Language :: Python :: 2.7",
+         "Operating System :: OS Independent",
+         "License :: OSI Approved :: MIT License",
+         "Development Status :: 1 - Planning",
+         "Topic :: Games/Entertainment :: Board Games"
+      ]
+     )
